7b9ab0aedc781908d92c2d5f37183f883e606d1d
OKwumi <oyekanmikhadijat@gmail.com>
2022-07-04 00:53:47 +0200

    secondcommit

---
+++ datatypes.js
@@ -0,0 +1,38 @@
+console.log(typeof 'basheeroh')
+console.log(typeof 20)
+let firstName
+console.log(firstName)
+console.log(typeof 'true')
+console.log(typeof null)
+
+let fName
+console.log(fName)
+let ag
+console.log(ag)
+let gravit
+console.log(gravit)
+let p
+console.log(p)
+
+let fiName = 'khodeejah'
+console.log(fiName)
+let a = '23'
+console.log(a)
+let gravity = 10
+console.log(gravity)
+let pi = 22/7
+console.log(pi)
+
+let firsName = 'KOOyekanmi'
+let lastName = 'Oyekanmi'
+let iMarried = 'False'
+let country = 'Nigeria'
+let agee = 22
+console.log(firsName, lastName, iMarried, country, agee)
+
+let name= 'Khodeejah', job= 'Student', live = 'Obaagun'
+console.log(name, job, live)
+
+
+
+
---
+++ exer3_day2.js
@@ -0,0 +1,64 @@
+ console.log(typeof 'basheeroh')
+console.log(typeof 20)
+let firstName
+console.log(firstName)
+console.log(typeof 'true')
+console.log(typeof null)
+
+let fName
+console.log(fName)
+let ag
+console.log(ag)
+let gravit
+console.log(gravit)
+let p
+console.log(p)
+
+let fiName = 'khodeejah'
+console.log(fiName)
+let a = '23'
+console.log(a)
+let gravity = 10
+console.log(gravity)
+let pi = 22/7
+console.log(pi)
+
+let firsName = 'KOOyekanmi'
+let lastName = 'Oyekanmi'
+let iMarried = 'False'
+let country = 'Nigeria'
+let agee = 22
+console.log(firsName, lastName, iMarried, country, agee)
+
+let name= 'Khodeejah', job= 'Student', live = 'Obaagun'
+console.log(name, job, live)
+
+
+let fullName = 'Oyekanmi Basheeroh Opeyemi'//string
+let age = '20'//number
+let isMarried = 'false'//boolean
+let emptyValue = null
+
+console.log(fullName, age, isMarried, emptyValue)
+
+let myAge = 'My age is 100'
+console.log(myAge)
+let yourAge = 'Your age is 200'
+console.log(yourAge)
+
+
+
+let string='Love is the best thing in this world. Some found their love and some are still looking for their love.'
+let pattern=/love/gi
+console.log(string.match(pattern))
+
+let string2='You cannot end a sentence with because because because is a conjunction'
+let pattern2=/because/gi
+console.log(string2.match(pattern2))
+
+const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+console.log(sentence.replace(/[%$@&#]/g,''))
+
+let r=5
+let b=6
+console.log(r+b)
